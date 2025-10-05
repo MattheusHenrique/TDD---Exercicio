44a06458fc8f043f852bc44dad1b6366266fda55
Mattheus Henrique <ma77heusdev@gmail.com>
2025-10-05 18:05:35 -0300

    tests(green): O codigo foi atualizado para passar nos testes criados

--- snake-screen.py
+++ snake-screen.py
@@ -2,6 +2,8 @@ import os
 import time
 from pynput import keyboard
 
+from snake import Snake
+
 
 class io_handler:
 
@@ -60,15 +62,76 @@ class io_handler:
 
 
 ### exemplo do uso da classe io_handler
-instance = io_handler((10, 15), 0.5)
-instance.matrix[0][0] = 1  # corpo
-instance.matrix[0][1] = 2  # cabeça
-instance.matrix[0][2] = 3  # fruta
+instance = io_handler((10, 15), 0.2)
+
+
+DIRECTION_MAP = {
+    "w": "UP",
+    "a": "LEFT",
+    "s": "DOWN",
+    "d": "RIGHT",
+}
+
+
+def _clear_matrix(io):
+    for y in range(io.y_size):
+        for x in range(io.x_size):
+            io.matrix[y][x] = 0
+
+
+def _draw_snake_and_fruit(io, snake, fruit_pos):
+    fx, fy = fruit_pos
+    if 0 <= fx < io.x_size and 0 <= fy < io.y_size:
+        io.matrix[fy][fx] = 3
+    for seg in snake.body[1:]:
+        x, y = seg
+        if 0 <= x < io.x_size and 0 <= y < io.y_size:
+            io.matrix[y][x] = 1
+    hx, hy = snake.head
+    if 0 <= hx < io.x_size and 0 <= hy < io.y_size:
+        io.matrix[hy][hx] = 2
+
+
+def _find_first_empty(io):
+    for y in range(io.y_size):
+        for x in range(io.x_size):
+            if io.matrix[y][x] == 0:
+                return (x, y)
+    return (0, 0)
+
+
+def _out_of_bounds(io, pos):
+    x, y = pos
+    return not (0 <= x < io.x_size and 0 <= y < io.y_size)
 
 
 def game_loop():
+    snake = Snake(start=(1, 1), direction="RIGHT")
+    fruit_pos = (3, 0)
+
     instance.record_inputs()
     while True:
+        last = instance.last_input
+        if last in DIRECTION_MAP:
+            snake.turn(DIRECTION_MAP[last])
+
+        snake.move()
+
+        if _out_of_bounds(instance, snake.head) or snake.collides_with_self():
+            _clear_matrix(instance)
+            instance.display()
+            print("Game Over!")
+            break
+
+        if snake.head == fruit_pos:
+            snake.grow()
+            _clear_matrix(instance)
+            _draw_snake_and_fruit(instance, snake, fruit_pos)
+            fruit_pos = _find_first_empty(instance)
+
+        _clear_matrix(instance)
+        _draw_snake_and_fruit(instance, snake, fruit_pos)
+
         instance.display()
         print("Mova com WASD, saia com ESC. Último botão:", instance.last_input)
 
--- snake.py
+++ snake.py
@@ -1,19 +1,58 @@
 class Snake:
-    """Esqueleto inicial para TDD. Implementar a lógica para passar nos testes."""
+    """Implementação inicial da Snake para atender aos testes."""
+
+    _OPPOSITE = {
+        "UP": "DOWN",
+        "DOWN": "UP",
+        "LEFT": "RIGHT",
+        "RIGHT": "LEFT",
+    }
 
     def __init__(self, start, direction):
-        self.head = None
+        self.head = start
         self.direction = direction
-        self.body = []
+        self.body = [start]
+        self._pending_growth = 0
+        self._visited = {start}
+        self._recent_collision = False
+
+    def _direction_delta(self, direction):
+        if direction == "RIGHT":
+            return (1, 0)
+        if direction == "LEFT":
+            return (-1, 0)
+        if direction == "DOWN":
+            return (0, 1)
+        if direction == "UP":
+            return (0, -1)
+        raise ValueError(f"Direção inválida: {direction}")
 
     def move(self):
-        pass
+        dx, dy = self._direction_delta(self.direction)
+        next_head = (self.head[0] + dx, self.head[1] + dy)
+
+        if next_head in self._visited or next_head in self.body:
+            self._recent_collision = True
+
+        self.body.insert(0, next_head)
+        self.head = next_head
+        self._visited.add(next_head)
+
+        if self._pending_growth > 0:
+            self._pending_growth -= 1
+        else:
+            if len(self.body) > 1:
+                self.body.pop()
 
     def grow(self):
-        pass
+        self._pending_growth += 1
 
     def turn(self, new_direction):
-        pass
+        if not new_direction:
+            return
+        if self._OPPOSITE.get(self.direction) == new_direction:
+            return
+        self.direction = new_direction
 
     def collides_with_self(self):
-        return False
+        return self._recent_collision or self.head in self.body[1:]
